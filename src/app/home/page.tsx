523d5a01c4628fc830cdf13513d06c531daea2c2
Abdul Majeed <johnharris5934@gmail.com>
2024-11-03 18:36:28 +0500

    first commit

---
+++ src/app/home/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React from "react";
+import Navbar from "../components/navbar";
+import HomeCenter from '../components/homecontent';
+
+function Home(){
+
+
+    return(
+
+        <div className='min-h-[100vh] w-full'>
+
+            <Navbar/>
+
+            <HomeCenter/>
+        
+
+          
+             
+        </div>
+    )
+
+
+    }
+
+   
+
+
+export default Home;
